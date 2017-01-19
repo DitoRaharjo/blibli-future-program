8d2d8f2a3ad3c3d7a92f5a7842114f44c5b929ee
Dito Raharjo <ditoraharjo@gmail.com>
2017-01-19 11:32:44 +0700

    git reset 2

--- sb-admin-2.js
+++ sb-admin-2.js
@@ -2,6 +2,8 @@ $(function() {
     $('#side-menu').metisMenu();
 });
 
+tambah commit 2;
+
 //Loads the correct sidebar on window load,
 //collapses the sidebar on window resize.
 // Sets the min-height of #page-wrapper to window size
